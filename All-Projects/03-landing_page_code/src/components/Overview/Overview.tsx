e79f697ce0f0f1c7b007ddff1f86b91d91c9962f
Vilberto02 <albertojulcapatricio@gmail.com>
2024-09-07 00:45:37 -0500

    Modificar proyecto de la landing page

--- All-Projects/03-landing_page_code/src/components/Overview/Overview.tsx
+++ All-Projects/03-landing_page_code/src/components/Overview/Overview.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 export function Overview() {
   return (
     <div className={style.overview}>
-      <div>
-        <h2>
+      <div className={style.container_description}>
+        <h2 className={style.title}>
           The best remote pair programming <br /> app on macOS and Windows
         </h2>
-        <p>
+        <p className={style.description}>
           Built from the ground up to deliver razor sharp screen sharing, crisp
           audio, and seamless remote control, so you can get into the flow with
           your teammates and do real work.
         </p>
-        <button>Start your free trial</button>
-        <span>
+        <button className={style.btn}>Start your free trial</button>
+        <span className={style.span}>
           {" "}
           <strong>Free for 14 days</strong>. No credit card
         </span>
@@ -23,6 +23,7 @@ export function Overview() {
           alt="overview"
           width="800"
           height="500"
+          priority={true}
         />
       </div>
       <div>
