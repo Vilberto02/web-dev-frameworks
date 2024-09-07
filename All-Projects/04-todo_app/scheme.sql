36c8256cb35a7b1e3f7fabdc3f7ba4a3c261f3fd
Vilberto02 <albertojulcapatricio@gmail.com>
2024-09-07 01:06:23 -0500

    Modificar archivo scheme para cambiar la sintaxis de algunas sentencias de mysql

--- All-Projects/04-todo_app/scheme.sql
+++ All-Projects/04-todo_app/scheme.sql
@@ -4,39 +4,39 @@ show databases;
 
 USE todo_app;
 
-CREATE TABLE users{
+CREATE TABLE users(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255),
-};
+);
 
 show tables;
 
-CREATE TABLE todos {
+CREATE TABLE todos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255),
-  completed BOOLEAN DEFAULT false,
+  completed BOOLEAN DEFAULT 0,
   user_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-};
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
 show tables;
 
-CREATE TABLE shared_todos {
+CREATE TABLE shared_todos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   todo_id INT,
   user_id INT,
   shared_with_id INT,
   FOREIGN KEY (todo_id) REFERENCES todos(id),
   FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (shared_with_id) REFERENCES users(id) ON DELETE CASCADE,
-};
+  FOREIGN KEY (shared_with_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
 show tables;
 
-INSERT INTP users (name, email, password) VALUES ("Beto", "userBeto@example.com", "userBeto");
-INSERT INTP users (name, email, password) VALUES ("Juan", "userJuan@example.com", "userJuan");
+INSERT INTO users (name, email, password) VALUES ("Beto", "userBeto@example.com", "userBeto");
+INSERT INTO users (name, email, password) VALUES ("Juan", "userJuan@example.com", "userJuan");
 
 SELECT * FROM users;
 
@@ -53,6 +53,6 @@ SELECT * FROM todos;
 
 INSERT INTO shared_todos (todo_id, user_id, shared_with_id)
 VALUES
-(1,1,2)
-(2,1,2)
-(3,1,2)
+(1,1,2),
+(2,1,2),
+(3,1,2);
