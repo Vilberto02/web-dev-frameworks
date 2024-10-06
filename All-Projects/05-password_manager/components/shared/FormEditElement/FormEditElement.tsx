7c82267b8345bb1740dd5e3474cba093e1e5e793
Vilberto02 <albertojulcapatricio@gmail.com>
2024-10-05 23:56:53 -0500

    Agregar Formulario de edición de contraseñas en Password Manager

--- All-Projects/05-password_manager/components/shared/FormEditElement/FormEditElement.tsx
+++ All-Projects/05-password_manager/components/shared/FormEditElement/FormEditElement.tsx
@@ -1,5 +1,292 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { formSchema } from "./FormEditElement.form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Copy } from "lucide-react";
+import { copyClipboard } from "@/lib/copyClipboard";
+import { Link } from "lucide-react";
+import { Shuffle } from "lucide-react";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { generatePassword } from "@/lib/generatePassword";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/hooks/use-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import { FormEditProps } from "./FormEditElement.types";
 
 export function FormEditElement({ dataElement }: FormEditProps) {
-  return <div>EDIT</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const router = useRouter();
+
+  const generateRandomPassword = () => {
+    const password = generatePassword();
+    form.setValue("password", password);
+  };
+
+  const updateUrl = () => {
+    form.setValue("urlWebsite", window.location.href);
+  };
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      typeElement: dataElement.typeElement,
+      isFavourite: dataElement.isFavourite,
+      name: dataElement?.name || "",
+      directory: dataElement?.directory || "",
+      username: dataElement?.username || "",
+      password: dataElement?.password || "",
+      urlWebsite: dataElement?.urlWebsite || "",
+      notes: dataElement?.notes || "",
+      userId: dataElement.userId,
+    },
+  });
+
+  // 2. Define a submit handler.
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/items/${dataElement.id}`, values);
+      toast({
+        title: "Item edited ✅",
+      });
+
+      router.push("/");
+    } catch (error) {
+      toast({
+        title: "Something wen strong",
+        variant: "destructive",
+      });
+    }
+  };
+
+  return (
+    <div>
+      Edit Page
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="md:grid-cols-2 gap-y-2 gap-x-4 grid"
+        >
+          <FormField
+            control={form.control}
+            name="typeElement"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>¿Qué tipo de elemento necesitas?</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a directory for your password"></SelectValue>
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="Inicio de sesión">
+                      Inicio de sesión
+                    </SelectItem>
+                    <SelectItem value="Tarjeta">Tarjeta</SelectItem>
+                    <SelectItem value="Identidad">Identidad</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="isFavourite"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  ¿Quieres seleccionar tu contraseña como favorita?
+                </FormLabel>
+                <div className="flex flex-row items-start space-x-3 space-y-0 p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>Marcar como favorito</FormLabel>
+                  </div>
+                </div>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre</FormLabel>
+                <FormControl>
+                  <Input {...field}></Input>
+                </FormControl>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="directory"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Directorio</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Elige el directorio"></SelectValue>
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="Social">Social</SelectItem>
+                    <SelectItem value="Arts">Arts</SelectItem>
+                    <SelectItem value="Shopping">Shopping</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Usuario</FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <Input {...field}></Input>
+                    <Copy
+                      className="absolute top-3 right-4 cursor-pointer"
+                      size={18}
+                      onClick={() => copyClipboard(field.value)}
+                    ></Copy>
+                  </div>
+                </FormControl>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="urlWebsite"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>URL Website</FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <Input {...field}></Input>
+                    <Link
+                      size={18}
+                      className="absolute right-2 top-3 cursor-pointer"
+                      onClick={updateUrl}
+                    ></Link>
+                  </div>
+                </FormControl>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex justify-between">
+                  Password
+                  <Shuffle
+                    className="cursor-pointer"
+                    size={18}
+                    onClick={generateRandomPassword}
+                  ></Shuffle>
+                </FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <Input
+                      {...field}
+                      type={showPassword ? "text" : "password"}
+                    ></Input>
+                    {showPassword ? (
+                      <EyeOff
+                        className="absolute top-3 right-10 cursor-pointer"
+                        size={18}
+                        onClick={() => setShowPassword(!showPassword)}
+                      ></EyeOff>
+                    ) : (
+                      <Eye
+                        className="absolute top-3 right-10 cursor-pointer"
+                        size={18}
+                        onClick={() => setShowPassword(!showPassword)}
+                      ></Eye>
+                    )}
+                    <Copy
+                      size={18}
+                      className="absolute top-3 right-3 cursor-pointer"
+                      onClick={() => copyClipboard(field.value)}
+                    ></Copy>
+                  </div>
+                </FormControl>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+          <div>
+            <div className="text-slate-400 flex items-center justify-between text-sm">
+              Autenticación TOTP
+              <p className="px-3 bg-green-700 text-white rounded-lg text-xs mr-5">
+                Premium
+              </p>
+            </div>
+            <Input disabled></Input>
+          </div>
+
+          <FormField
+            control={form.control}
+            name="notes"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Notes</FormLabel>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          ></FormField>
+          <div></div>
+
+          <Button type="submit">Guardar</Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
